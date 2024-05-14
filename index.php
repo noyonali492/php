fbb3686c4f8c52f169d2305507a69ee2a9224cef
noyonali492 <noyonahmed492@gmail.com>
2024-05-14 23:12:06 +0600

    php syntex

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
-<?php
-    echo("Noyon");
+<!doctype html>
+<html>
+<head>
+    <title>PHP Syntax</title>
+    <style>
+        .phpcoding{
+            width:900px
+            padding:20px;
+            background:#ddd;
+            margin: 0 auto;
+            min-height:400px;
+        }
+        .haderoption, .foteroption{background:#ddd, color: #fff;text-align:center;}
+    </style>
+</head>
+<body>
+    <section class="phpcoding">
+        <div class="haderoption">
+            <h2>Php Fundamental Trining</h2>
+        </div>
+            <?php
+                echo"i love php";
 
-?>
+            ?>
+
+        <div class="foteroption">
+            <h2>Noyonali.com</h2>
+        </div>
+
+    </section>
+
+</body>
+</html>
